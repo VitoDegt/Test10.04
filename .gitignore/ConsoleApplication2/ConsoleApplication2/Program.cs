637dea224cc120a2c4822cb05f34a3159cecf5af
VitoDegt <geret91@mail.ru>
2017-04-10 21:38:16 +0300

    Test 10.04

---
+++ .gitignore/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    struct City
+    {
+        string Name;
+        int Population;
+    }
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            City[] cities = new City();
+            string text = "Kharkiv=1431000,Kiev=280400,LasVegas=603400";
+            char[] delimiterChars = { '=', ',' };
+            string[] words = text.Split(delimiterChars);
+
+            foreach (var item in words)
+            {
+                 
+            }
+
+            int theBiggesCity = 0;
+            int longesNameCity = 0;
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                
+            }
+            
+
+        }
+    }
+}
